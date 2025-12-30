5e4705c730a5ba7f9135e3f79b540061fb4ca8a1
최윤영pc <pc@10.67.48.80>
2025-12-30 17:27:41 +0900

    update masking rule

--- src/main/java/com/example/demo/util/MaskingUtil.java
+++ src/main/java/com/example/demo/util/MaskingUtil.java
@@ -10,29 +10,35 @@ public class MaskingUtil {
      * - 2글자: 첫 번째 글자 마스킹 (예: 이슬 -> 이*)
      * - 3글자 이상: 첫 번째와 마지막 글자 제외 마스킹 (예: 홍길동 -> 홍*동)
      */
-    public static String maskName(String name) {
-        if (name == null || name.isEmpty()) {
-            return "";
-        }
-        int len = name.length();
-        
-        if (len == 2) {
-            // 2글자: 두 번째 글자 마스킹
-            return name.charAt(0)+"*" ;// 예) 이슬 -> 이*
-        } else if (len >= 3) {
-            // 3글자 이상: 첫 번째와 마지막 글자 제외 마스킹
-            char first = name.charAt(0);
-            char last = name.charAt(len - 1);
-            
-            // 중간 글자 수만큼 '*' 생성
-            String middleMask = "*".repeat(len - 2); 
-            
-            return first + middleMask + last; // 예) 홍길동 -> 홍*동, 이순신 -> 이*신
-        } else {
-            // 1글자 등 예외적인 경우 (마스킹하지 않거나 전부 * 처리)
-            return name; // 1글자인 경우 그대로 반환
-        }
-    }
+	public static String maskName(String name) {
+	    if (name == null || name.isEmpty()) {
+	        return "";
+	    }
+
+	    // 탈퇴 회원은 마스킹하지 않음
+	    if ("탈퇴 회원".equals(name)) {
+	        return name;
+	    }
+
+	    int len = name.length();
+
+	    if (len == 2) {
+	        // 2글자: 두 번째 글자 마스킹
+	        return name.charAt(0)+"*" ;// 예) 이슬 -> 이*
+	    } else if (len >= 3) {
+	        // 3글자 이상: 첫 번째와 마지막 글자 제외 마스킹
+	        char first = name.charAt(0);
+	        char last = name.charAt(len - 1);
+
+	        // 중간 글자 수만큼 '*' 생성
+	        String middleMask = "*".repeat(len - 2);
+
+	        return first + middleMask + last; // 예) 홍길동 -> 홍*동, 이순신 -> 이*신
+	    } else {
+	        // 1글자 등 예외적인 경우 (마스킹하지 않거나 전부 * 처리)
+	        return name; // 1글자인 경우 그대로 반환
+	    }
+	}
 
     /**
      * 아이디 마스킹 규칙:
